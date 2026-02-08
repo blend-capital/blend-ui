2e088f5a5870627c58933dcf040e44bcf51788aa
mootz12 <mootz12@protonmail.com>
2026-02-08 14:49:57 -0500

    chore: improve logging when fetching token balances

--- src/external/token.ts
+++ src/external/token.ts
@@ -27,13 +27,25 @@ export async function getTokenBalance(
   if (rpc.Api.isSimulationSuccess(result)) {
     let resultScVal = (result as rpc.Api.SimulateTransactionSuccessResponse).result?.retval;
     if (resultScVal == undefined) {
-      console.error(`Error: unable to fetch balance for token: ${token_id}`);
+      console.error(
+        `Error: unable to fetch balance for token, no return value detected: ${token_id}, ${result?.result}`
+      );
       return BigInt(0);
     } else {
       return scValToNative(resultScVal);
     }
   } else {
-    console.error(`Error: unable to fetch balance for token: ${token_id}`);
-    return BigInt(0);
+    let error = rpc.Api.isSimulationError(result)
+      ? result.error
+      : rpc.Api.isSimulationRestore(result)
+      ? 'data must be restored'
+      : 'unknown error';
+    if (error.includes('Error(Contract, #13)')) {
+      // this is an expected error and means the user does not have a trustline for the token, so we can return a balance of 0 without logging an error
+      return BigInt(0);
+    } else {
+      console.error(`Error: unable to fetch balance for token: ${token_id}, error: ${error}`);
+      return BigInt(0);
+    }
   }
 }
